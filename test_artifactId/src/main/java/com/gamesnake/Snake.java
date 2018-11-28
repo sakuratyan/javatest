821a8dad1f4a3e584323aa816e4962c217b6cb6a
sakuratyan <1105429691@qq.com>
2018-11-28 23:27:47 +0800

    gameSnake

--- test_artifactId/src/main/java/com/gamesnake/Snake.java
+++ test_artifactId/src/main/java/com/gamesnake/Snake.java
@@ -1,5 +1,7 @@
 package com.gamesnake;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
 import java.util.ArrayList;
 
@@ -8,19 +10,41 @@ import java.util.ArrayList;
  */
 public class Snake {
 
+    class Node implements Grid {
+        Point p = null;
+        Color c = null;
+
+        Node(Point p, Color c) {
+            this.p = p;
+            this.c = c;
+        }
+
+        void draw(Graphics g) {
+            Color oc = g.getColor();
+            g.setColor(this.c);
+            g.fillRect(GRID_OFFSET.x + (this.p.x - 1) * BLOCK_SIZE, GRID_OFFSET.y + (this.p.y - 1) * BLOCK_SIZE,
+                    BLOCK_SIZE, BLOCK_SIZE);
+            g.setColor(oc);
+        }
+    }
+
     ArrayList<Point> sl = new ArrayList<>();
-    
-    Snake(Point p){
+
+    Snake(Point p) {
         sl.add(p);
     }
 
-    int eat(Egg g){
-        //snake.add(e)
+    int eat(Egg g) {
+        // snake.add(e)
         return 0;
     }
-    
-    ArrayList<Point> getLocation(){
+
+    ArrayList<Point> getLocation() {
         return sl;
     }
-    
+
+    int getlength() {
+        return sl.size();
+    }
+
 }
--- test_artifactId/src/main/java/com/gamesnake/Yard.java
+++ test_artifactId/src/main/java/com/gamesnake/Yard.java
@@ -5,13 +5,20 @@ import java.awt.event.*;
 import java.util.ArrayList;
 
 /**
- * Yard
+ * interface Grid
  */
-public class Yard extends Frame {
-    private static final int ROWS = 40;
-    private static final int COLS = 50;
-    private static final int BLOCK_SIZE = 20;
-    Point startP = new Point(BLOCK_SIZE, BLOCK_SIZE * 2);
+interface Grid {
+    public static final int ROWS = 40;
+    public static final int COLS = 50;
+    public static final int BLOCK_SIZE = 20;
+    public static Point GRID_OFFSET = new Point(BLOCK_SIZE, BLOCK_SIZE * 2);
+
+}
+
+/**
+ * class Yard
+ */
+public class Yard extends Frame implements Grid {
 
     public void launch() {
         this.setLocation(0, 0);
@@ -29,31 +36,23 @@ public class Yard extends Frame {
     public void paint(Graphics g) {
         Color oc = g.getColor();
         g.setColor(Color.gray);
-        g.fillRect(startP.x, startP.y, COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE);
+        g.fillRect(GRID_OFFSET.x, GRID_OFFSET.y, COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE);
 
         // 背景横竖线
         g.setColor(Color.darkGray);
         for (int i = 1; i < ROWS; i++) {
-            g.drawLine(startP.x, startP.y + i * BLOCK_SIZE, startP.x + COLS * BLOCK_SIZE, startP.y + i * BLOCK_SIZE);
+            g.drawLine(GRID_OFFSET.x, GRID_OFFSET.y + i * BLOCK_SIZE, GRID_OFFSET.x + COLS * BLOCK_SIZE,
+                    GRID_OFFSET.y + i * BLOCK_SIZE);
         }
         for (int i = 1; i < COLS; i++) {
-            g.drawLine(startP.x + i * BLOCK_SIZE, startP.y, startP.x + i * BLOCK_SIZE, startP.y + ROWS * BLOCK_SIZE);
+            g.drawLine(GRID_OFFSET.x + i * BLOCK_SIZE, GRID_OFFSET.y, GRID_OFFSET.x + i * BLOCK_SIZE,
+                    GRID_OFFSET.y + ROWS * BLOCK_SIZE);
         }
 
-        g.drawString("str", 100, 100);
         // snake
-        Snake snake = new Snake(new Point(5, 5));
-        ArrayList<Point> sl = snake.getLocation();
-        g.setColor(Color.blue);
-        this.drawPoint(g, sl.get(0));
 
         g.setColor(oc);
     }
 
-    int drawPoint(Graphics g, Point p) {
-        g.fillRect(startP.x + (p.x - 1) * BLOCK_SIZE, startP.y + (p.y - 1) * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-
-        return 0;
-    }
 
 }
